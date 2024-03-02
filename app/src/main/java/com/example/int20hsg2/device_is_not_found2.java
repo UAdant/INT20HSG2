321ae6d4ffb33f6a8cea0def2204a83702183db4
D3sp1n <alexd001115@gmail.com>
2024-03-02 23:57:16 +0200

    refresh activity_device_is_not_found2.xml

--- app/src/main/java/com/example/int20hsg2/device_is_not_found2.java
+++ app/src/main/java/com/example/int20hsg2/device_is_not_found2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DeviceIsNotFound extends AppCompatActivity {
+public class device_is_not_found2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_is_not_found);
+        setContentView(R.layout.activity_device_is_not_found2);
     }
 }
