cc7c31952a9d9fad03cce8bd6c3d476972bfa2b3
Mashun-KeK <zadoroznijruslan5@gmail.com>
2024-03-03 06:29:35 +0200

    Merge remote-tracking branch 'origin/master' 60% disane
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/int20hsg2/DoneSecurity.java
+++ app/src/main/java/com/example/int20hsg2/DoneSecurity.java
@@ -1,6 +1,9 @@
 package com.example.int20hsg2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DoneSecurity extends AppCompatActivity {
+    private Button btnBack3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_done_security);
@@ -19,6 +24,17 @@ public class DoneSecurity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnBack3=findViewById(R.id.btnBack3);
+
+        btnBack3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Перехід на екран MainActivity
+                Intent intent = new Intent(DoneSecurity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
         });
     }
 }
--- app/src/main/java/com/example/int20hsg2/device_is_not_found2.java
+++ app/src/main/java/com/example/int20hsg2/device_is_not_found2.java
@@ -1,23 +1,39 @@
 package com.example.int20hsg2;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class device_is_not_found2 extends AppCompatActivity {
+    private Button btnBack1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_is_not_found2);
+        btnBack1=findViewById(R.id.btnBack1);
+
+        btnBack1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View animation) {
+                // Перехід на екран MainActivity
+                Intent intent1 = new Intent(device_is_not_found2.this, MainActivity.class);
+                startActivity(intent1);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
 
         Button sendRequestButton = findViewById(R.id.sendRequestButton);
         sendRequestButton.setOnClickListener(new View.OnClickListener() {
